9cded0ca9fd86ce3c0589e5fd40925d06c5c907e
seonghun <shson1211@tukorea.ac.kr>
2022-05-18 21:12:01 +0900

    hello world

---
+++ WP Final Project/WP Final Project/Main.cpp
@@ -0,0 +1,59 @@
+#include <windows.h>
+#include <tchar.h>
+
+HINSTANCE g_hInst;
+TCHAR lpszClass[] = TEXT("WP Final Project");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	if (!hPrevInstance) {
+		WndClass.cbClsExtra = 0;
+		WndClass.cbWndExtra = 0;
+		WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+		WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+		WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+		WndClass.hInstance = hInstance;
+		WndClass.lpfnWndProc = (WNDPROC)WndProc;
+		WndClass.lpszClassName = lpszClass;
+		WndClass.lpszMenuName = NULL;
+		WndClass.style = CS_HREDRAW | CS_VREDRAW;
+		RegisterClass(&WndClass);
+	}
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, 0, 0, 1024, 768, NULL, (HMENU)NULL, hInstance, NULL);		//--- 윈도우 크기 변경 가능
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, 0, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	static TCHAR str[50] = L"Hello, world!";
+
+	switch (iMessage) {
+
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		TextOut(hdc,0,0,str,lstrlen(str));
+
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
