ec9b5cbb7d5a3e0c9d513570768d1574f469a1c6
Vallhup <ths1220ths@naver.com>
2022-05-28 21:16:24 +0900

    배경화면, 골대 추가

--- WP Final Project/WP Final Project/Main.cpp
+++ WP Final Project/WP Final Project/Main.cpp
@@ -1,5 +1,8 @@
 #include <windows.h>
 #include <tchar.h>
+#include <atlimage.h>
+
+#pragma comment (lib, "msimg32.lib")
 
 HINSTANCE g_hInst;
 TCHAR lpszClass[] = TEXT("WP Final Project");
@@ -26,7 +29,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 		WndClass.style = CS_HREDRAW | CS_VREDRAW;
 		RegisterClass(&WndClass);
 	}
-	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, 0, 0, 1000, 700, NULL, (HMENU)NULL, hInstance, NULL);		//--- 윈도우 크기 변경 가능
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, 0, 0, 1016, 779, NULL, (HMENU)NULL, hInstance, NULL);		//--- 윈도우 크기 변경 가능
 	ShowWindow(hWnd, nCmdShow);
 
 	while (GetMessage(&Message, 0, 0, 0)) {
@@ -38,14 +41,26 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
-	HDC hdc;
+	HDC hdc, memdc;
 	PAINTSTRUCT ps;
 
+	static CImage BackGround;
+	static CImage GoalPostR, GoalPostL;
+
 	switch (iMessage) {
+	case WM_CREATE:
+		BackGround.Load(_T("BackGround.png"));
+		GoalPostR.Load(_T("GoalPost - R.png"));
+		GoalPostL.Load(_T("GoalPost - L.png"));
+
+		break;
+
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 
-
+		BackGround.BitBlt(hdc, 0, 0, 1000, 740, 0, 0, SRCCOPY);
+		GoalPostR.TransparentBlt(hdc, 930, 570, 62, 109, 0, 0, 62, 109, RGB(255, 255, 255));
+		GoalPostL.TransparentBlt(hdc, 30, 570, 62, 109, 0, 0, 62, 109, RGB(255, 255, 255));
 
 		EndPaint(hWnd, &ps);
 
